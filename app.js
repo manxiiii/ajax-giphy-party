aa62689db6cbd99908f29b5a273cd1492ab4b67d
Manxi Wang <mxwang0102@gmail.com>
2023-05-05 09:54:17 -0700

    tests passed

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Let's get this party started!");
+document.addEventListener('DOMContentLoaded',function(){
+  let form = document.querySelector('form');
+  let input = document.getElementById('input');
+  let remove = document.getElementById('remove');
+  let results = document.getElementById('results');
+
+  remove.addEventListener('click',function(e){
+    while (results.firstChild) {
+      results.removeChild(results.firstChild);
+    }
+  });
+  form.addEventListener("submit",async function(e){
+    e.preventDefault();
+    let url = `http://api.giphy.com/v1/gifs/search?q=${input.value}&api_key=kPFWE25SibiNT2U54mE1FtKEVI18IdDW`
+    let response = await axios.get(url);
+    let res = response.data;
+    let numResults = res.data.length;
+
+    let randomIdx;
+    if (numResults) {
+      randomIdx = Math.floor(Math.random() * numResults);
+
+    }
+    let image = document.createElement('img');
+    image.setAttribute("src", res.data[randomIdx].images.original.url);
+    results.appendChild(image);
+    e.target.reset();
+  });
+})
